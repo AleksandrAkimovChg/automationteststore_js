c75d23173b96ffe28825f6852ee1b064bb71d1b7
AleksandrAkimovChg <104619642+AleksandrAkimovChg@users.noreply.github.com>
2023-06-25 21:30:39 +0300

    new ver (#3)
    Co-authored-by: Aleksandr Akimov <alexander.akimov.chg@gmail.com>

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  viewportWidth: 1920,
-  viewportHeight: 1080,
-  chromeWebSecurity: false,
-  e2e: {
+viewportWidth: 1920,
+viewportHeight: 1080,
+chromeWebSecurity: false,
+defaultCommandTimeout: 7000,
+e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+        require('cypress-mochawesome-reporter/plugin')(on);
     },
-  },
-  video: false,
-    reporter: 'junit',
-    reporterOptions: {
-        mochaFile: 'reports/test-results-[hash].xml',
+},
+video: false,
+reporter: 'cypress-mochawesome-reporter',
+reporterOptions: {
+        embeddedScreenshots: true,
+        charts: true,
+        reportFilename: 'mochawesome',
+        reportDir: 'mochawesomeReports',
+        overwrite: true,
+        mochaFile: 'reports/junit/test-results-[hash].xml'
     },
 });
